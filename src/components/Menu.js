4e8ff35c55fc134de7e2b5b499602390b9db3b4f
vidhi1213 <vidhipatel1414@gmail.com>
2023-05-17 10:38:19 +0530

    first commit

---
+++ src/components/Menu.js
@@ -0,0 +1,30 @@
+import "../App.css";
+import { useContext } from "react";
+import { GameStateContext } from "../helpers/Contexts";
+
+function Menu() {
+  const { setGameState, setUserName } = useContext(
+    GameStateContext
+  );
+  return (
+    <div className="Menu">
+      <label>Enter Your Name:</label>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setGameState("playing");
+        }}
+      >
+        Start Quiz
+      </button>
+    </div>
+  );
+}
+
+export default Menu;
